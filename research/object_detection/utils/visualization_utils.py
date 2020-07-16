c643071061eca74a1dd8022c408e36033f691031
Alvarole <leandro0807@live.com>
2020-07-16 15:03:01 -0300

    removed another print

--- research/object_detection/utils/visualization_utils.py
+++ research/object_detection/utils/visualization_utils.py
@@ -1014,7 +1014,6 @@ def visualize_boxes_and_labels_on_image_array(
   # Create a display string (and color) for every box location, group any boxes
   # that correspond to the same location.
   new_xml = False
-  print('IM HERRE')
 
   box_to_display_str_map = collections.defaultdict(list)
   box_to_color_map = collections.defaultdict(str)
